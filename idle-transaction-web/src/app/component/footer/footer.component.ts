ffb058d1e7f1441306ee89f81667790216fb28de
david <zhaodavid007@gmail.com>
2023-11-07 17:48:49 +0800

    提交代码

--- idle-transaction-web/src/app/component/footer/footer.component.ts
+++ idle-transaction-web/src/app/component/footer/footer.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-footer',
-  templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.less']
+    selector: 'app-footer',
+    templateUrl: './footer.component.html',
+    styleUrls: ['./footer.component.less']
 })
 export class FooterComponent implements OnInit {
+    footerMenu = ['首页', '闲置大厅', '消息通知', '关于我们', '个人中心']
+    constructor() { }
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
 
 }
