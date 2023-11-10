7bdbecd201008b41a7c01291a3e774cd28176700
david <zhaodavid007@gmail.com>
2023-11-10 19:44:43 +0800

    提交代码

--- idle-transaction-web/src/app/pages/idle-hall/idle-hall.module.ts
+++ idle-transaction-web/src/app/pages/idle-hall/idle-hall.module.ts
@@ -3,11 +3,17 @@ import { IdleHallComponent } from './idle-hall.component';
 import { IdleHallRoutingModule } from './idle-hall.routing';
 import { ShareModule } from 'src/share/share.module';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
 @NgModule({
     declarations: [IdleHallComponent],
     imports: [
         IdleHallRoutingModule,
         CommonModule,
+        NzLayoutModule,
+        NzMenuModule,
+        ShareModule,
     ],
     exports: [
         IdleHallComponent,
